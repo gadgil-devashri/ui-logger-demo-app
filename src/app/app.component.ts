f9dcfd6426fa71a5b920b9dbb7fa88852353db28
Muthu Selvam <muthu.softwar@gmail.com>
2022-04-17 11:01:36 -0400

    Imported logger framework to angular app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import LoggerWrapper from '../app/npm-logger-package';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+
+    // Initializing the ui logger framework
+    LoggerWrapper.init({
+      appId: 'ui-logger-demo',
+      currentSession: 'test',
+      advnaced: false,
+      eventType: 'FUN_DRINKS',
+      eventSubType: 'LIST_OF_DRINKS'
+    })
+    // Info level logger 
+    LoggerWrapper.info({
+      level: 'info',
+      appId: 'ui-logger-demo',
+      eventType: 'FUN_DRINKS',
+      eventSubType: 'LIST_OF_DRINKS',
+      eventSource: {
+        component: 'app.component',
+        subComponent: 'app.component.html',
+        stepName: "Angular app loadded successfully",
+        businessCapability: "display login",
+        businessFunctionality: "display login"
+      },
+      eventAttributes: {
+        // Add custom attributes events
+       url: 'http://localhost:4200/login'
+      }
+    })
+  }
   title = 'ui-logger-demo';
 }
--- src/app/user-auth/user-auth.component.ts
+++ src/app/user-auth/user-auth.component.ts
@@ -2,9 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { DatePipe } from '@angular/common';
 import { Router } from '@angular/router';
-// import { LoggerWrapper } from '../../logger-package'
-// import LoggerWrapper from '../../logger-package'
-
 @Component({
   selector: 'app-user-auth',
   templateUrl: './user-auth.component.html',
@@ -18,32 +15,7 @@ export class UserAuthComponent implements OnInit {
   });
   constructor(private formBuilder: FormBuilder,private datePipe: DatePipe, private router:Router) { }
 
-  ngOnInit(): void {
-  // Logger init
-  /* LoggerWrapper.init({
-    appId: 'ui-logger-demo',
-    currentSession: 'test',
-    advnaced: false,
-    eventType: 'FUN_DRINKS',
-    eventSubType: 'LIST_OF_DRINKS'
-  })
-  LoggerWrapper.info({
-    level: 'info',
-    appId: 'ui-logger-demo',
-    eventType: 'FUN_DRINKS',
-    eventSubType: 'LIST_OF_DRINKS',
-    eventSource: {
-      component: 'user-auth.component',
-      subComponent: 'user-authentication-module',
-      stepName: "user trying to login",
-      businessCapability: "logger",
-      businessFunctionality: "log trace"
-    },
-    eventAttributes: {
-     url: 'www.uncc.edu'
-    }
-  })*/
-  } 
+  ngOnInit(): void {}
 
   onSubmit(): void {
     // userAuthForm data here
